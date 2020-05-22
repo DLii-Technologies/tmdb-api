3ec72ae36bc230badae229b475f9d09c60e4a9d3
David Ludwig <davidludwigii@gmail.com>
2020-05-22 17:53:33 -0500

    Remove commented code

--- test/init.ts
+++ test/init.ts
@@ -45,22 +45,6 @@ export let auth = {
 	}
 	run();
 })();
-// authentication.createRequestToken(auth.api_key).then((result) => {
-// 	auth.request_token = result.request_token;
-// 	return authentication.login(auth.api_key, auth.request_token, auth.username, auth.password);
-// }).then((result) => {
-// 	return authentication.createSession(auth.api_key, result.request_token);
-// }).then((result) => {
-// 	auth.session_id = result.session_id;
-// 	return account.getDetails(auth.api_key, auth.session_id);
-// }).then((result) => {
-// 	auth.account_id = result.id;
-// 	run(); // Start the tests
-// }).catch(e => {
-// 	console.error(e);
-// 	console.error("Initialization failed!");
-// 	process.exit(1);
-// });
 
 // -------------------------------------------------------------------------------------------------
 
